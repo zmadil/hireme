e333c80aca87af4c2186e90308bcbca319beb2eb
Zain <zain@Zains-MacBook-Pro.local>
2026-05-16 20:49:49 -0500

    adding testononial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
+import Testimonials from "@/components/Testimonials";
 import HireMe from "@/components/HireMe";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
@@ -18,6 +19,7 @@ export default function Home() {
         <Projects />
         <Skills />
         <Experience />
+        <Testimonials />
         <HireMe />
         <Contact />
       </main>
